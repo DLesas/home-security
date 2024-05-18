7369be943f93b3634e3457ba1486f3d23ef8dc96
DLesas <dlesas@hotmail.com>
2024-05-18 10:50:59 +0100

    chore: added some network functions
    chore: added device code (which might need some edits)
    
    chore: made a rudiementary backend

--- backend/door_sensors.py
+++ backend/door_sensors.py
@@ -1,21 +1,48 @@
+from operator import index
 import pandas as pd
 import os
-import socket
+import network_functions
 import requests
 import json
 import threading
 import time
+from playsound import playsound
 
-sensors = [{"name": "", "delay": 0.3}]
-armed = False
+sensors = [
+    {"name": "[Pico] livingRoom-frenchDoor1", "delay": 0.3, "mac": "28-CD-C1-0F-33-3A"},
+    {"name": "[Pico] livingRoom-frenchDoor2", "delay": 0.3, "mac": "28-CD-C1-0F-31-62"},
+    {"name": "[Pico] backDoor", "delay": 0.3, "mac": "28-CD-C1-0F-34-5E"},
+    {"name": "[Pico] frontDoor", "delay": 0.3, "mac": "28-CD-C1-0F-2B-25"}
+]
+armed = True
+playing = False
+fileDir = os.path.dirname(os.path.realpath(__file__))
+logFolder = os.path.join(fileDir, "logs")
 
 
 def writeToFile(json: dict, name: str):
-    df = pd.DataFrame(json)
-    df.to_csv(name, mode="a", header=not os.path.exists(name))
+    print(f"{name}: {json}")
+    json["date"] = pd.to_datetime("now").strftime("%Y-%m-%d %H:%M:%S")
+    df = pd.DataFrame(json, index=[0])
+    df.to_csv(
+        os.path.join(logFolder, name),
+        mode="a",
+        header=not os.path.exists(os.path.join(logFolder, name)),
+        index=False,
+    )
+
+
+def alarm():
+    global playing
+    playsound(
+        r"C:\Users\Dimitri\Documents\Javascript\home_security\backend\Treasure.wav"
+    )
+    playing = False
 
 
 def sensorWork(addr: str, sensorDict: dict):
+    global playing
+    print("started work")
     while True:
         res = requests.get(addr)
         resJson = json.loads(res.content)
@@ -23,13 +50,23 @@ def sensorWork(addr: str, sensorDict: dict):
         if resJson["temp"] > 45:
             continue
             # warn about potential fire?
-        if resJson["door"] == "open" and armed == True:
-            continue
+        if resJson["door"] == "open" and armed == True and playing == False:
+            playing = True
+            soundT = threading.Thread(target=alarm)
+            soundT.start()
             # warn about potential intruder
         time.sleep(sensorDict["delay"])
 
 
 def main():
     for sensor in sensors:
-        ip = socket.gethostbyname(sensor["name"])
+        # ip = socket.gethostbyname(sensor["name"])
+        ip = network_functions.find_ip_by_mac(sensor["mac"])
+        print(f'found ip of {sensor["name"]}: {ip}')
         addr = f"http://{ip}/"
+        t = threading.Thread(target=sensorWork, args=(addr, sensor))
+        t.start()
+
+
+if __name__ == "__main__":
+    main()
