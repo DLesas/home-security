cc532b37b02f31023ceef5f5ee4de94229a4ee56
DLesas <dlesas@hotmail.com>
2024-05-18 11:17:49 +0100

    chore: corrected door sensor file
    chore: create network function file for the micro devices

--- backend/devices/door_sensor.py
+++ backend/devices/door_sensor.py
@@ -3,112 +3,97 @@ import socket
 from time import sleep
 from picozero import pico_temp_sensor, pico_led
 import machine
-import urequests
-from machine import Pin
 import json
 import _thread
+from network_functions import get_local_ip, get_broadcast_address, get_network_mask
 
-switch = Pin(22, Pin.IN, Pin.PULL_UP)
-
+switch = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_UP)
 
 ssid = "BT-P8A5PT"
-password = 'hARMLTPd7caeHf'
-count = 0
+password = "hARMLTPd7caeHf"
+keep_alive_interval = 60
+
+ip = get_local_ip()
+mask = get_network_mask(ip)
+broadcastAddress = get_broadcast_address(ip, mask)
+
 
 def connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
-    while wlan.isconnected() == False:
-        print('Waiting for connection...')
+    while not wlan.isconnected():
+        print("Waiting for connection...")
         sleep(1)
     ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
+    print(f"Connected on {ip}")
     return ip
-    
+
+
 def open_socket(ip):
-    # Open a socket
-    print(ip)
     address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
     connection.listen(1)
-    print(connection)
+    print(f"Listening on {ip}:80")
     return connection
 
-def webpage(temperature, state, switch):
-    #Template HTML
-    html = f"""
-            <!DOCTYPE html>
-            <html>
-            <form action="./lighton">
-            <input type="submit" value="Light on" />
-            </form>
-            <form action="./lightoff">
-            <input type="submit" value="Light off" />
-            </form>
-            <p>LED is {state}</p>
-            <p>switch value is {switch}</p>
-            <p>Temperature is {temperature}</p>
-            </body>
-            </html>
-            """
-    html = f'{switch}'
-    return str(html)
 
-def send_html(jsondata, connection):
-    print('accepting')
+def serve(connection):
+    while True:
+        try:
+            client, addr = connection.accept()
+            print("Connection from", addr)
+            _thread.start_new_thread(handle_client, (client,))
+        except KeyboardInterrupt:
+            print("Server stopped by user")
+            break
+
+
+def handle_client(client):
     try:
-        client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
         request = request.split()[1]
-        #    pico_led.on()
-        #    state = 'ON'
-        #elif request =='/lightoff?':
-        #    pico_led.off()
-        #    state = 'OFF'
-        
-        client.send('HTTP/1.0 200 OK\r\nContent-type: text/json\r\n\r\n')
-        client.send(jsondata)
-        client.close()
-    except IndexError:
-            pass
+        temperature = pico_temp_sensor.temp
+        door_state = "open" if switch.value() else "closed"
+        data = {"door_state": door_state, "temperature": temperature}
+        response = json.dumps(data)
+        client.send("HTTP/1.1 200 OK\r\n")
+        client.send("Content-Type: application/json\r\n\r\n")
+        client.send(response)
     finally:
         client.close()
-        print(' >> Connection closed.')
 
-def serve(connection):
-    #Start a web server
-    state = 'OFF'
-    pico_led.off()
-    temperature = 0
+
+def light_control():
     while True:
-        pico_led.off()
         val = switch.value()
         if val == 1:
             pico_led.on()
-            senState = 'open'
-            
         else:
             pico_led.off()
-            senState = 'closed'
-        temperature = pico_temp_sensor.temp
-        data = {'door': senState, 'temp': temperature}
-        #res = urequests.post('http://192.168.43.94:5000/webhook', headers = {'content-type': 'application/json'}, data=json.dumps(data))
-        #print(res.content)
-        jsondata = json.dumps(data)
-        #jsonresults = json.loads(res)
-        #print(jsonresults)
-        print('ready to accept')
-        send_html(jsondata, connection)
-        
+        sleep(0.5)
+
+
+def send_keep_alive():
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while True:
+        try:
+            udp_socket.sendto(
+                b"Keep alive", (broadcastAddress, 60000)
+            )  # Change IP and port as needed
+            sleep(keep_alive_interval)
+        except Exception as e:
+            print("Error:", e)
 
 
 try:
     ip = connect()
     connection = open_socket(ip)
-    serve(connection)
-except:
+    _thread.start_new_thread(serve, (connection,))
+    _thread.start_new_thread(light_control, ())
+    _thread.start_new_thread(send_keep_alive, ())
+except Exception as e:
+    print("Error:", e)
     machine.reset()
-
