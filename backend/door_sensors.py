56279ea8166e99a854a418e70f83ea8e0cecde0f
DLesas <dlesas@hotmail.com>
2024-05-20 12:19:21 +0100

    chore: working example

--- backend/door_sensors.py
+++ backend/door_sensors.py
@@ -1,4 +1,4 @@
-from operator import index
+from flask import Flask, request, jsonify
 import pandas as pd
 import os
 import network_functions
@@ -6,7 +6,9 @@ import requests
 import json
 import threading
 import time
+import multiprocessing
 from playsound import playsound
+from flask_cors import CORS
 
 sensors = [
     {
@@ -34,16 +36,31 @@ sensors = [
         "potentialIP": "192.168.1.75",
     },
 ]
+
+latestLog = {
+    "livingRoom-frenchDoor1": None,
+    "livingRoom-frenchDoor2": None,
+    "backDoor": None,
+    "frontDoor": None,
+}
+
+latestLog2 = [
+    {"name": None, "time": None, "status": None, "temp": None},
+    {"name": None, "time": None, "status": None, "temp": None},
+    {"name": None, "time": None, "status": None, "temp": None},
+    {"name": None, "time": None, "status": None, "temp": None}
+]
+
 armed = False
 playing = False
 fileDir = os.path.dirname(os.path.realpath(__file__))
 logFolder = os.path.join(fileDir, "logs")
 
 
-def writeToFile(json: dict, name: str):
-    json["date"] = pd.to_datetime("now").strftime("%d-%m-%Y %H:%M:%S")
+def writeToFile(data: dict, name: str):
+    data["date"] = pd.to_datetime("now").strftime("%d-%m-%Y %H:%M:%S")
     date = pd.to_datetime("now").strftime("%d_%m_%Y")
-    df = pd.DataFrame(json, index=[0])
+    df = pd.DataFrame(data, index=[0])
     df.to_csv(
         os.path.join(logFolder, f'{date + "_" + name}'),
         mode="a",
@@ -61,13 +78,15 @@ def alarm():
 
 
 def sensorWork(addr: str, sensorDict: dict):
-    global playing
+    global playing, armed, latestLog
     print("started work")
     while True:
         try:
             res = requests.get(addr)
             resJson = json.loads(res.content)
             print(f"{sensorDict['name']}: {resJson}")
+            name = sensorDict["name"].split("] ")[1]
+            latestLog[name] = resJson
             t = threading.Thread(
                 target=writeToFile, args=(resJson, sensorDict["name"] + ".csv")
             )
@@ -75,9 +94,10 @@ def sensorWork(addr: str, sensorDict: dict):
             if resJson["temperature"] > 45:
                 continue
                 # warn about potential fire?
-            if resJson["door_state"] == "open" and armed == True and playing == False:
+            if resJson["door_state"] == "open" and armed and not playing:
+                print('ALARM!!!')
                 playing = True
-                soundT = threading.Thread(target=alarm)
+                soundT =  threading.Thread(target=alarm)
                 soundT.start()
             # warn about potential intruder
             time.sleep(sensorDict["delay"])
@@ -87,17 +107,10 @@ def sensorWork(addr: str, sensorDict: dict):
 
 
 def main():
-    #ip = network_functions.get_local_ip()
-    #mask = network_functions.get_network_mask(ip)
-    #broadcastAddress = network_functions.get_broadcast_address(ip, mask)
-    #network_functions.refresh_arp_cache(broadcastAddress)
-    #network_functions.send_direct_arp_requests(broadcastAddress)
-    #network_functions.scan_network_with_nmap(broadcastAddress, mask, ip)
     for sensor in sensors:
-        # ip = socket.gethostbyname(sensor["name"])
         ip = network_functions.find_ip_by_mac(sensor["mac"])
         print(f'found ip of {sensor["name"]}: {ip}')
-        if ip == None:
+        if ip is None:
             print(f'could not find ip of {sensor["name"]}, falling back to potentialIP')
             ip = sensor["potentialIP"]
         addr = f"http://{ip}/"
@@ -105,5 +118,34 @@ def main():
         t.start()
 
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/arm', methods=['GET'])
+def arm():
+    global armed
+    armed = True
+    return jsonify({"armed": armed})
+
+@app.route('/disarm', methods=['GET'])
+def disarm():
+    global armed
+    armed = False
+    return jsonify({"armed": armed})
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": armed})
+
+@app.route('/logs', methods=['GET'])
+def logs():
+    return jsonify(latestLog)
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000)
+
 if __name__ == "__main__":
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True
+    flask_thread.start()
     main()
