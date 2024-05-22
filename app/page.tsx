bff1ba4d1ff00cb7c2431b2d53e223e144bf2a1d
DLesas <dlesas@hotmail.com>
2024-05-22 22:43:14 +0100

    chore: got image recognition working, and a few changes to webpage but nothing concrete yet

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,29 @@ import { useEffect, useState } from 'react'
 import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/card'
 import { Divider } from '@nextui-org/divider'
 import { cn } from '@/lib/utils'
-
+import humanizeDuration from 'humanize-duration'
 const example = {
-  backDoor: {
-    date: '18-05-2024 19:49:05',
-    door_state: 'closed',
-    temperature: 31.2577,
-  },
-  frontDoor: {
-    date: '18-05-2024 19:49:05',
-    door_state: 'open',
-    temperature: 29.85327,
-  },
-  'livingRoom-frenchDoor1': {
-    date: '18-05-2024 19:49:05',
-    door_state: 'closed',
-    temperature: 33.59842,
-  },
-  'livingRoom-frenchDoor2': {
-    date: '18-05-2024 19:49:05',
-    door_state: 'closed',
-    temperature: 41.5569,
+  House: {
+    'Back door': {
+      status: 'closed',
+      temp: 28.44887,
+      time: 'Wed, 22 May 2024 00:08:59 GMT',
+    },
+    'Dining room - French door': {
+      status: 'closed',
+      temp: 28.91698,
+      time: 'Wed, 22 May 2024 00:08:59 GMT',
+    },
+    'Front door': {
+      status: 'closed',
+      temp: 28.91698,
+      time: 'Wed, 22 May 2024 00:08:59 GMT',
+    },
+    'Living room - French door': {
+      status: 'closed',
+      temp: 31.72584,
+      time: 'Wed, 22 May 2024 00:08:59 GMT',
+    },
   },
 }
 
@@ -33,14 +35,18 @@ function SensorCard({
   name,
   door_state,
   temperature,
+  time,
 }: {
   name: string
   door_state: string
   temperature: number
+  time: string
 }) {
+  const date = new Date(time)
+  date.setHours(date.getHours())
   return (
     <Card
-      className={`max-w-[400px] ${door_state === 'open' ? 'bg-red-500' : ''}  `}
+      className={`${door_state === 'open' ? 'bg-red-500' : ''}  col-span-4`}
     >
       <CardHeader className="flex gap-3">
         <div className="flex flex-col">
@@ -55,6 +61,14 @@ function SensorCard({
       <CardFooter>
         <p> {temperature} </p>
       </CardFooter>
+      <div>
+        {humanizeDuration(new Date() - date, {
+          units: ['s', 'ms'],
+          round: true,
+        })}
+      </div>
+      <div>{new Date().toLocaleTimeString()}</div>
+      <div>{date.toLocaleTimeString()}</div>
     </Card>
   )
 }
@@ -102,39 +116,66 @@ export default function Index() {
   // ]
 
   useEffect(() => {
-    const intervalId = setInterval(async () => {
-      try {
-        const response = await fetch(
-          `http://${process.env.NEXT_PUBLIC_IP}:5000/logs`,
-          {
-            method: 'GET',
-          }
-        )
-        const data: data = await response.json()
-        setData(data)
-        // You might need to handle the response based on the actual content type
-      } catch (error) {
-        console.error('Error fetching sensor data:', error)
-      }
-    }, 500)
+    const socket = new WebSocket(
+      'ws://' + process.env.NEXT_PUBLIC_IP + ':5000' + '/echo'
+    )
+    socket.addEventListener('message', (ev) => {
+      setData(JSON.parse(ev.data))
+    })
+    // const intervalId = setInterval(async () => {
+    //   try {
+    //     const response = await fetch(
+    //       `http://${process.env.NEXT_PUBLIC_IP}:5000/logs`,
+    //       {
+    //         method: 'GET',
+    //       }
+    //     )
+    //     const data: data = await response.json()
 
-    // Clean up intervals on component unmount
+    //     setData(data)
+    //     // You might need to handle the response based on the actual content type
+    //   } catch (error) {
+    //     console.error('Error fetching sensor data:', error)
+    //   }
+    // }, 10)
+
+    // // Clean up intervals on component unmount
+    // return () => {
+    //   clearInterval(intervalId)
+    // }
     return () => {
-      clearInterval(intervalId)
+      socket.close()
     }
   }, [])
 
   return (
-    <div className="flex flex-row justify-center">
-      <div className="flex w-8/12 flex-1 flex-col items-center justify-center gap-20">
-        {Object.entries(data).map(([key, value]) => (
-          <SensorCard
-            key={key}
-            name={key}
-            door_state={value.door_state}
-            temperature={value.temperature}
-          />
-        ))}
+    <div className="flex justify-center">
+      <div className="grid w-8/12 grid-cols-8 grid-rows-6 gap-4">
+        <div className="col-span-8 lg:col-span-2">
+          <div>
+            <div className="flex flex-row justify-center">
+              {Object.entries(data).map(([key, value]) => (
+                <div className="grid grid-cols-8 gap-8">
+                  {' '}
+                  {key}
+                  {Object.entries(value).map(([nestedKey, nestedValue]) => (
+                    <SensorCard
+                      key={nestedKey}
+                      name={nestedKey}
+                      door_state={nestedValue.status}
+                      temperature={nestedValue.temp}
+                      time={nestedValue.time}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="col-span-3 col-start-3 row-span-3">2</div>
+        <div className="col-span-2 col-start-6 row-span-3">3</div>
+        <div className="col-span-3 col-start-5 row-span-3 row-start-4">4</div>
+        <div className="col-span-2 col-start-3 row-span-3 row-start-4">5</div>
       </div>
     </div>
   )
