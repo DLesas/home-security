517be3ac5d2c248a225c14fb2a754ac4a06630a7
DLesas <dlesas@hotmail.com>
2024-06-11 19:10:24 +0100

    draft

--- lib/socket.ts
+++ lib/socket.ts
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = 'http://192.168.5.157:5000';
 // const URL = 'http://192.168.226.94:5000';
-const URL = "http://" + process.env.NEXT_PUBLIC_IP + ":5000"
+const URL_VPN = "https://" + process.env.NEXT_PUBLIC_VPN_IP + ":5000"
 
 
-export const socket = io(URL);
+export const socket = io(URL_VPN);
