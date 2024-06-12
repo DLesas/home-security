bea071b00e565cd28c102e9f9eaa67e9615a6046
DLesas <dlesas@hotmail.com>
2024-06-12 18:15:15 +0100

    lots of changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    reloadOnOnline: true,
-    register: true,         // register the PWA service worker
-    skipWaiting: true,
-    // ... other options you like
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,      // Enable React strict mode for improved error handling
-    swcMinify: true,
-  };
-  
-  module.exports = withPWA(nextConfig);
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  reloadOnOnline: true,
+  register: true, // register the PWA service worker
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  // ... other options you like
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true,
+}
+
+module.exports = withPWA(nextConfig)
+
+
+module.exports = nextConfig
