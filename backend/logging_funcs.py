426cf979712378a751c890d49869863e375b5a26
DLesas <dlesas@hotmail.com>
2024-06-27 15:47:16 +0100

    chore: clean up

--- backend/logging_funcs.py
+++ backend/logging_funcs.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import os
-from devices import sensors
 import datetime
+
 # import network_functions
-import numpy as np
-from typing import List
-from alarm_funcs import turnOnAlarms, turnOffAlarms
 
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
@@ -15,10 +12,10 @@ issuesFolder = os.path.join(logFolder, "issues")
 
 
 def writeToFile(data: dict, name: str):
-    
+
     if not os.path.exists(sensorFolder):
         os.makedirs(sensorFolder)
-        print('created sensor log path')
+        print("created sensor log path")
     data["date"] = pd.to_datetime("now").strftime("%d-%m-%Y %H:%M:%S")
     date = pd.to_datetime("now").strftime("%d_%m_%Y")
     filename = os.path.join(sensorFolder, f'{date + "_" + name}.csv')
@@ -30,14 +27,15 @@ def writeToFile(data: dict, name: str):
         index=False,
     )
 
-def issuesToFile(data:dict):
+
+def issuesToFile(data: dict):
 
     if not os.path.exists(issuesFolder):
         os.makedirs(issuesFolder)
-        print('created sensor log path')
+        print("created sensor log path")
     data["date"] = pd.to_datetime("now").strftime("%d-%m-%Y %H:%M:%S")
     date = pd.to_datetime("now").strftime("%d_%m_%Y")
-    filename = os.path.join(issuesFolder, f'{date}.csv')
+    filename = os.path.join(issuesFolder, f"{date}.csv")
     df = pd.DataFrame(data, index=[0])
     df.to_csv(
         filename,
@@ -46,9 +44,10 @@ def issuesToFile(data:dict):
         index=False,
     )
 
+
 def readIssueFile(date: datetime) -> pd.DataFrame | None:
     date.strftime("%d_%m_%Y")
-    filename = os.path.join(issuesFolder, f'{date}.csv')
+    filename = os.path.join(issuesFolder, f"{date}.csv")
     if os.path.exists(filename):
         return pd.read_csv(filename)
     else:
