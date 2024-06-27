08d9390d4a226bd5ff882b6e9fb7180c9ce1af69
DLesas <dlesas@hotmail.com>
2024-06-27 15:47:35 +0100

    chore: added warning before full alarm

--- backend/alarm_funcs.py
+++ backend/alarm_funcs.py
@@ -2,10 +2,19 @@ from devices import alarms
 import requests as r
 import win32com.client as win32
 import boto3
+import time
 
 test = False
 
 # Create an SNS client
+
+triggering = False  # This is a global variable that is used to determine if the alarm should be triggered. If it is true, the alarm will be triggered. If it is false, the alarm sequence will be turned off entierly.
+
+
+def turnOffAlarmsUseCase():
+    global triggering
+    triggering = False
+    turnOffAlarms()
 
 
 def turnOffAlarms():
@@ -39,6 +48,28 @@ def turnOnAlarms():
                     print(e)
 
 
+def turnOnAlarmsUseCase():
+    global triggering
+    triggering = True
+    print("alarm temporarily on for warning")
+    turnOnAlarms()
+    initial_t_end = time.time() + 0.5
+    while time.time() < initial_t_end:
+        if triggering is False:
+            turnOffAlarms()
+            break
+    turnOffAlarms()
+    print("alarm off for 5 seconds for warning")
+    seconnd_t_end = time.time() + 5
+    while time.time() < seconnd_t_end:
+        if triggering is False:
+            break
+    if triggering:
+        print("alarm fully on")
+        turnOnAlarms()
+    triggering = False
+
+
 def send_mail(body: str, subject: str):
     # Ensure the Outlook object is created in the current thread context
     outlook = win32.Dispatch("outlook.application")
@@ -47,32 +78,29 @@ def send_mail(body: str, subject: str):
         mail.To = "dlesas@hotmail.com"
     else:
         mail.To = "dlesas@hotmail.com; deborah@leabeater.co.uk; david.leabeater57@icloud.com; victoria.leabeater@gmail.com"
-    #mail.To = "dlesas@hotmail.com"
+    # mail.To = "dlesas@hotmail.com"
     mail.Subject = subject
     mail.Body = body
     mail.Send()
 
 
 def send_SMS(body: str):
-    sns_client = boto3.client('sns')
-    phones = ['+447810570861', '+447825514688', '+447771594191', '+447854972536'] # '07854972536'
-    testPhone = '+447810570861'
+    sns_client = boto3.client("sns")
+    phones = [
+        "+447810570861",
+        "+447825514688",
+        "+447771594191",
+        "+447854972536",
+    ]  # '07854972536'
+    testPhone = "+447810570861"
     if test:
         try:
-            response = sns_client.publish(
-                PhoneNumber=testPhone,
-                Message=body
-            )
+            response = sns_client.publish(PhoneNumber=testPhone, Message=body)
         except Exception as e:
             print(e)
     else:
         for phone in phones:
             try:
-                response = sns_client.publish(
-                    PhoneNumber=phone,
-                    Message=body
-                )
+                response = sns_client.publish(PhoneNumber=phone, Message=body)
             except Exception as e:
                 print(e)
-
-
