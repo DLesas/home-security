bac7a9d1e05635c89813352a8d4adc23e0b9f6da
DLesas <dlesas@hotmail.com>
2024-07-24 23:10:36 +0100

    chore: made a start on rewriting backend to node js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,100 @@
 // Import the 'express' module
-import express, { Request, Response } from 'express';
+import { EntityId } from "redis-om";
+import { doorSensorStateRepository } from "./redis/doorSensorState";
+import { doorSensorsRepository } from "./redis/doorSensor";
+import express, { Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { db } from "./db/db";
+import { generalLogsTable } from "./db/schema/generalLogs";
+import { Placeholder, SQL } from "drizzle-orm";
+import { configRepository } from "./redis/config";
 
 // Create an Express application
 const app = express();
+app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 8080;
 
-const port = 5000;
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+io.emit("some-event", { some: "data" });
 
 // Define a route for the root path ('/')
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   // Send a response to the client
-  res.send('Hello, TypeScript + Node.js + Express!');
+  res.send("Hello, TypeScript + Node.js + Express!");
+});
+
+app.post("/api/v1/logs", async (req, res) => {
+  const { state, temperature }: {
+    state: "open" | "closed";
+    temperature: number;
+  } = req.body;
+  const clientIp = req.ip!;
+  insertGeneralLog({
+    endpoint: "api/v1/logs",
+    action: "post",
+    connection: "http",
+    clientIp,
+    userAgent: req.headers["user-agent"],
+  });
+});
+
+app.get("/songs", async (req, res) => {
+  const snesorStates = await doorSensorStateRepository.search().returnAll();
+  res.send(snesorStates);
 });
 
 // Start the server and listen on the specified port
-app.listen(port, () => {
+server.listen(port, () => {
   // Log a message when the server is successfully running
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+async function insertGeneralLog(
+  log: {
+    connection: "http" | "socket";
+    endpoint: string;
+    action:
+      | "post"
+      | "get"
+      | "delete"
+      | "patch";
+    clientIp: string;
+    id?: number | undefined;
+    dateTime?:
+      | Date
+      | null
+      | undefined;
+    userAgent?:
+      | string
+      | null
+      | undefined;
+  },
+) {
+  await db.insert(generalLogsTable).values(log);
+}
+
+async function dealWithDoorSensorUpdate(
+  { state, temperature, ip }: {
+    state: "open" | "closed";
+    temperature: number;
+    ip: string;
+  },
+) {
+  const sensor = await doorSensorsRepository.search().where("ipAddress").eq(
+    ip,
+  ).returnFirst();
+  const currentState = await doorSensorStateRepository.search().where(
+    "ipAddress",
+  ).eq(ip).returnFirst();
+  const config = await configRepository.search().returnFirst();
+  if (currentState && currentState.armed && state === "open") {
+    // trigger alarm
+  }
+
+}
