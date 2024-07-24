2896e3e566d078bc6b7eebfd4c70da9302cafd7b
DLesas <dlesas@hotmail.com>
2024-07-24 23:11:21 +0100

    chore: added a postgres schema and a way to intreact with our postgres db

---
+++ backend/src/db/schema/sensorLogs.ts
@@ -0,0 +1,32 @@
+import {
+  bigserial,
+  integer,
+  numeric,
+  pgEnum,
+  pgTable,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { doorSensorsTable } from "./doorSensors";
+import { index } from "drizzle-orm/pg-core";
+
+export const doorSensorStateEnum = pgEnum("state", [
+  "open",
+  "closed",
+  "unknown",
+]);
+
+export const sensorLogsTable = pgTable("sensorLogs", {
+  id: bigserial("id", { mode: "number" }).primaryKey(),
+  sensorId: integer("sensorId").notNull().references(
+    () => doorSensorsTable.id,
+    { onDelete: "cascade" },
+  ),
+  state: doorSensorStateEnum("state").notNull(),
+  temperature: numeric("temperature", { precision: 5, scale: 2 }).notNull(),
+  dateTime: timestamp("dateTime").defaultNow(),
+}, (table) => {
+  return {
+    dateTimeIdx: index("dateTimeIdx").on(table.dateTime),
+  };
+});
