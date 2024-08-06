f6b8fbb93004934096d29e441415b7cf98e9e57b
DLesas <dlesas@hotmail.com>
2024-08-06 20:34:02 +0100

    chore: include new postgres tables into migration

--- backend/src/db/schema/index.ts
+++ backend/src/db/schema/index.ts
@@ -1,16 +1,24 @@
-import {
-  doorSensorStateEnum,
-  sensorLogsTable,
-} from "./sensorLogs";
+import { doorSensorStateEnum, sensorLogsTable } from "./sensorLogs";
 import { doorSensorsTable } from "./doorSensors";
 import { buildingTable as buildingsTable } from "./buildings";
+import { eventLogsTable } from "./eventLogs";
+import { actionEnum, connectionEnum, generalLogsTable } from "./generalLogs";
 
+export const actionEnums = actionEnum;
+export const connectionEnums = connectionEnum;
+
+export const generalLogTable = generalLogsTable;
+export const eventLogTable = eventLogsTable;
 export const doorSensorState = doorSensorStateEnum;
 export const sensorLogTable = sensorLogsTable;
 export const doorSensorTable = doorSensorsTable;
 export const buildingTable = buildingsTable;
 
 export const db = {
+  actionEnums,
+  connectionEnums,
+  generalLogTable,
+  eventLogTable,
   doorSensorState,
   sensorLogTable,
   doorSensorTable,
