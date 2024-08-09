0a591b1ae0043bddbf5e19442ad0daa11c9192b3
DLesas <dlesas@hotmail.com>
2024-08-09 18:46:47 +0100

    chore: renamed generalLogs table to accessLogs, continued improved DoorSensorUpdate function

--- backend/src/db/schema/index.ts
+++ backend/src/db/schema/index.ts
@@ -2,12 +2,12 @@ import { doorSensorStateEnum, sensorLogsTable } from "./sensorLogs";
 import { doorSensorsTable } from "./doorSensors";
 import { buildingTable as buildingsTable } from "./buildings";
 import { eventLogsTable } from "./eventLogs";
-import { actionEnum, connectionEnum, generalLogsTable } from "./generalLogs";
+import { accessLogsTable, actionEnum, connectionEnum } from "./accessLogs";
 
 export const actionEnums = actionEnum;
 export const connectionEnums = connectionEnum;
 
-export const generalLogTable = generalLogsTable;
+export const generalLogTable = accessLogsTable;
 export const eventLogTable = eventLogsTable;
 export const doorSensorState = doorSensorStateEnum;
 export const sensorLogTable = sensorLogsTable;
