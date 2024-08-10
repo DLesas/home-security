aa2a7d01ae895decd99e1ee38be43b6f6f221a49
DLesas <dlesas@hotmail.com>
2024-08-10 13:48:00 +0100

    chore: tidy up + add jsDoc to various functions

--- backend/src/notifiy.ts
+++ backend/src/notifiy.ts
@@ -1,9 +1,31 @@
 // TODO: finish off notification system
 
+import { db } from "./db/db";
+import { eventLogsTable, type insertEventLog } from "./db/schema/eventLogs";
+
 export function raiseWarningNotification() {
   console.log("warning");
 }
 
 export function raiseCriticalNotification() {
   console.log("critical");
+}
+export async function raiseEvent(
+  type: insertEventLog["type"],
+  message: insertEventLog["message"],
+) {
+  await db.insert(eventLogsTable).values({
+    type: type,
+    message: message,
+  });
+  switch (type) {
+    case "critical":
+      raiseCriticalNotification();
+      break;
+    case "warning":
+      raiseWarningNotification();
+      break;
+    default:
+      break;
+  }
 }
