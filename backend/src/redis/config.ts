7740a2a917b6fbb4766e8f66084f34df48d03157
DLesas <dlesas@hotmail.com>
2024-08-12 11:05:17 +0100

    chore: started work on notification system

--- backend/src/redis/config.ts
+++ backend/src/redis/config.ts
@@ -4,11 +4,13 @@ import { redis } from ".";
 const configSchema = new Schema("config", {
   sensorWarningTemparature: { type: "number" },
   sensorCriticalTemparature: { type: "number" },
+  pushTokens: { type: "string[]" },
 });
 
 export interface Config {
   sensorWarningTemparature: number;
   sensorCriticalTemparature: number;
+  pushTokens: string[];
 }
 
 export const configRepository = new Repository(configSchema, redis);
