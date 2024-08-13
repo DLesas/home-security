22d6c0bd08d111445592ef9ba5e62f0e847ba8ea
DLesas <dlesas@hotmail.com>
2024-08-13 16:47:30 +0100

    refactor: added express middleware for logging access instead of doing it individually
    perf: removed unneccesary imports
    
    style: improved naming of functions/variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,27 @@
 // Import the 'express' module
-import { EntityId } from "redis-om";
-import {
-  type doorSensorState,
-  doorSensorStateRepository,
-} from "./redis/doorSensorState";
+import { type doorSensor, doorSensorRepository } from "./redis/doorSensors";
 import express, { Request, Response } from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { db } from "./db/db";
 import { accessLogsTable } from "./db/schema/accessLogs";
-import { type selectEventLog } from "./db/schema/eventLogs";
 import { checkSensorState, checkSensorTemperature } from "./sensorFuncs";
 import { raiseError } from "./errorHandling";
 
 // Create an Express application
 const app = express();
 app.use(express.json());
+app.use(async (req, res, next) => {
+  const clientIp = req.ip!;
+  await db.insert(accessLogsTable).values({
+    endpoint: req.baseUrl + req.path,
+    queryString: req.query.toString(),
+    action: req.method as "GET" | "POST" | "DELETE" | "PUT",
+    connection: "http",
+    clientIp,
+    userAgent: req.headers["user-agent"],
+  });
+});
 const server = http.createServer(app);
 const io = new Server(server);
 const port = 8080;
@@ -66,20 +72,13 @@ app.post("/api/v1/logs", async (req, res) => {
     temperature: number;
   } = req.body;
   const clientIp = req.ip!;
-  await db.insert(accessLogsTable).values({
-    endpoint: "api/v1/logs",
-    action: "post",
-    connection: "http",
-    clientIp,
-    userAgent: req.headers["user-agent"],
-  });
   await DoorSensorUpdate({ state, temperature, ip: clientIp });
   await emitNewData();
 });
 
 app.get("/api/v1/state", async (req, res) => {
-  const snesorStates = await doorSensorStateRepository.search().returnAll();
-  res.send(snesorStates);
+  const sensorStates = await doorSensorRepository.search().returnAll();
+  res.send(sensorStates);
 });
 
 // Start the server and listen on the specified port
@@ -95,23 +94,19 @@ async function DoorSensorUpdate(
     ip: string;
   },
 ) {
-  const currentState = await doorSensorStateRepository.search().where(
+  const currentState = await doorSensorRepository.search().where(
     "ipAddress",
-  ).eq(ip).returnFirst() as doorSensorState | null;
+  ).eq(ip).returnFirst() as doorSensor | null;
   if (currentState === null) {
     raiseError();
     return;
   }
-  const previousState = { ...currentState };
-  checkSensorState(previousState, state);
-  checkSensorTemperature(temperature, previousState);
+  checkSensorState(currentState, state);
+  checkSensorTemperature(temperature, currentState);
   currentState.state = state;
   currentState.temperature = temperature;
   currentState.date = new Date();
-  await doorSensorStateRepository.save(currentState);
-}
-
-async function changeSensorStatus() {
+  await doorSensorRepository.save(currentState);
 }
 
 async function emitNewData() {
