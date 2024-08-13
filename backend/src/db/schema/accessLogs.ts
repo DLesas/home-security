4f947ba05a668d645d30f28cb2ece2f78ef02512
DLesas <dlesas@hotmail.com>
2024-08-13 16:43:31 +0100

    feat: added capturing of query strings

--- backend/src/db/schema/accessLogs.ts
+++ backend/src/db/schema/accessLogs.ts
@@ -8,12 +8,13 @@ import {
 } from "drizzle-orm/pg-core";
 import { index } from "drizzle-orm/pg-core";
 
-export const actionEnum = pgEnum("actions", ["get", "post", "delete", "patch"]);
+export const actionEnum = pgEnum("actions", ["GET", "POST", "DELETE", "PUT"]);
 export const connectionEnum = pgEnum("connection", ["http", "socket"]);
 
 export const accessLogsTable = pgTable("accessLogs", {
   id: bigserial("id", { mode: "number" }).primaryKey(),
   endpoint: varchar("endpoint", { length: 256 }).notNull(),
+  queryString: varchar("queryString", { length: 2048 }),
   action: actionEnum("action").notNull(),
   connection: connectionEnum("connection").notNull(),
   clientIp: varchar("clientIp", { length: 256 }).notNull(),
