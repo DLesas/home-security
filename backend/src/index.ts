0c96ce6eec2a6b9c25c0ff102adc594bc737f447
DLesas <dlesas@hotmail.com>
2024-08-16 16:45:27 +0100

    feat: added a sane json return to clients to all endpoints.
    docs: updated comments in varioud places
    
    feat: added logging every time door sensor state changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,19 @@
 // Import the 'express' module
 import { type doorSensor, doorSensorRepository } from "./redis/doorSensors";
+import { integer } from "drizzle-orm/pg-core";
 import express, { Request, Response } from "express";
+import https from "https";
 import http from "http";
 import { Server } from "socket.io";
 import { db } from "./db/db";
 import { accessLogsTable } from "./db/schema/accessLogs";
-import { checkSensorState, checkSensorTemperature } from "./sensorFuncs";
+import {
+  changeSensorStatus,
+  checkSensorState,
+  checkSensorTemperature,
+} from "./sensorFuncs";
 import { raiseError } from "./errorHandling";
+import { sensorLogsTable } from "./db/schema/sensorLogs";
 
 // Create an Express application
 const app = express();
@@ -21,7 +28,18 @@ app.use(async (req, res, next) => {
     clientIp,
     userAgent: req.headers["user-agent"],
   });
+  next();
 });
+
+// const options = {
+//   key: fs.readFileSync("./test_key.key"),
+//   cert: fs.readFileSync("./test_cert.crt"),
+//   ca: fs.readFileSync("./test_ca.crt"),
+//   requestCert: false,
+//   rejectUnauthorized: false,
+// };
+
+// const server = https.createServer(options, app);
 const server = http.createServer(app);
 const io = new Server(server);
 const port = 8080;
@@ -30,39 +48,71 @@ io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript + Node.js + Express!");
-});
-
-app.post("/api/v1/buildings/:building/sensors/arm", async (req, res) => {
+// Route to arm all sensors in a specific building
+app.post("/api/v1/buildings/:building/arm", async (req, res) => {
   const building = req.params.building;
-  // Logic to arm all sensors in the specified building
-  res.send(`All sensors in building ${building} have been armed.`);
+  const sensors = await doorSensorRepository.search().where(
+    "building",
+  ).eq(building).returnAll() as doorSensor[];
+  await changeSensorStatus(sensors, true);
+  await emitNewData();
+  res.send({
+    status: "success",
+    message: `All sensors in building ${building} have been armed.`,
+  });
 });
 
 // Route to disarm all sensors in a specific building
-app.post("/api/v1/buildings/:building/sensors/disarm", async (req, res) => {
+app.post("/api/v1/buildings/:building/disarm", async (req, res) => {
   const building = req.params.building;
-  // Logic to disarm all sensors in the specified building
-  res.send(`All sensors in building ${building} have been disarmed.`);
+  const sensors = await doorSensorRepository.search().where(
+    "building",
+  ).eq(building).returnAll() as doorSensor[];
+  await changeSensorStatus(sensors, false);
+  await emitNewData();
+  res.send({
+    status: "success",
+    message: `All sensors in building ${building} have been disarmed.`,
+  });
 });
 
+// Route to arm a specific sensor
 app.post(
-  "/api/v1/buildings/:building/sensors/:sensor/arm",
+  "/api/v1/sensors/:sensor/arm",
   async (req, res) => {
-    const { building, sensor } = req.params;
-    // Logic to arm the specific sensor in the specified building
-    res.send(`Sensor ${sensor} in building ${building} has been armed.`);
+    const { sensor } = req.params;
+    const sensors = [
+      await doorSensorRepository.search().where("externalID").eq(sensor)
+        .returnFirst() as doorSensor,
+    ];
+    await changeSensorStatus(sensors, true);
+    await emitNewData();
+    res.send({
+      status: "success",
+      message: `${sensors[0].name} sensor in ${
+        sensors[0].building
+      } has been armed.`,
+    });
   },
 );
 
-// Route to disarm a specific sensor in a specific building
+// Route to disarm a specific sensor
 app.post(
-  "/api/v1/buildings/:building/sensors/:sensor/disarm",
+  "/api/v1/sensors/:sensor/disarm",
   async (req, res) => {
-    const { building, sensor } = req.params;
-    // Logic to disarm the specific sensor in the specified building
-    res.send(`Sensor ${sensor} in building ${building} has been disarmed.`);
+    const { sensor } = req.params;
+    const sensors = [
+      await doorSensorRepository.search().where("externalID").eq(sensor)
+        .returnFirst() as doorSensor,
+    ];
+    await changeSensorStatus(sensors, false);
+    await emitNewData();
+    res.send({
+      status: "success",
+      message: `${sensors[0].name} sensor in ${
+        sensors[0].building
+      } has been disarmed.`,
+    });
   },
 );
 
@@ -74,6 +124,7 @@ app.post("/api/v1/logs", async (req, res) => {
   const clientIp = req.ip!;
   await DoorSensorUpdate({ state, temperature, ip: clientIp });
   await emitNewData();
+  res.send({ status: "success", message: "new state saved" });
 });
 
 app.get("/api/v1/state", async (req, res) => {
@@ -106,9 +157,15 @@ async function DoorSensorUpdate(
   currentState.state = state;
   currentState.temperature = temperature;
   currentState.date = new Date();
+  db.insert(sensorLogsTable).values({
+    sensorId: parseInt(currentState.externalID),
+    state: state,
+    temperature: temperature.toString(),
+  });
   await doorSensorRepository.save(currentState);
 }
 
 async function emitNewData() {
+  // TODO: decide on funcion to emit new data
   io.emit("newData", new Date());
 }
