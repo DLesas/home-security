8ed2dab4e39ce26ad6deff89d6ec09adfe2ef11b
DLesas <dlesas@hotmail.com>
2024-08-23 16:03:54 +0100

    refactor: removed unneccesary alarm redis properties

--- backend/src/redis/alarms.ts
+++ backend/src/redis/alarms.ts
@@ -2,27 +2,25 @@ import { Repository, Schema } from "redis-om";
 import { redis } from ".";
 
 const alarmSchema = new Schema("alarms", {
-  name: { type: "string" },
-  playing: { type: "boolean" },
-  building: { type: "string" },
-  ipAddress: { type: "string" },
-  onAddress: { type: "string" },
-  offAddress: { type: "string" },
-  macAddress: { type: "string" },
-  created: { type: "date" },
+	name: { type: "string" },
+	externalID: { type: "string" },
+	playing: { type: "boolean" },
+	building: { type: "string" },
+	ipAddress: { type: "string" },
+	macAddress: { type: "string" },
+	created: { type: "date" },
 });
 
 export const alarmRepository = new Repository(alarmSchema, redis);
 
 export interface Alarm {
-  name: string;
-  playing: boolean;
-  building: string;
-  ipAddress: string;
-  onAddress: string;
-  offAddress: string;
-  macAddress: string;
-  created: Date;
+	name: string;
+	externalID: number;
+	playing: boolean;
+	building: string;
+	ipAddress?: string;
+	macAddress?: string;
+	created: Date;
 }
 
 await alarmRepository.createIndex();
