abfde22f7613fbf2eb57b79a7a8a067e7ee4f27d
DLesas <dlesas@hotmail.com>
2024-08-23 16:03:54 +0100

    refactor: removed unneccesary alarm redis properties

--- backend/src/alarmFuncs.ts
+++ backend/src/alarmFuncs.ts
@@ -7,22 +7,17 @@ import { type Alarm, alarmRepository } from "./redis/alarms";
  * @param {"on" | "off"} state - The state to change the alarms to.
  * @return {Promise<any[]>} - A promise that resolves to an array of results from the promises of sending the http requests to trigger the alarms.
  */
-export async function changeAlarmState(
-  alarms: Alarm[] | [],
-  state: "on" | "off",
-) {
-  const triggerPromises = [];
-  const savePromises = [];
-  for (let index = 0; index < alarms.length; index++) {
-    const element = alarms[index];
-    element.playing = state === "on" ? true : false;
-    savePromises.push(alarmRepository.save(element));
-    triggerPromises.push(
-      fetch(state === "on" ? element.onAddress : element.offAddress),
-    );
-  }
-  const saveRes = await Promise.all(savePromises);
-  const triggerRes = await Promise.all(triggerPromises);
-  const results = saveRes.concat(triggerRes);
-  return results;
+export async function changeAlarmState(alarms: Alarm[] | [], state: "on" | "off") {
+	const triggerPromises = [];
+	const savePromises = [];
+	for (let index = 0; index < alarms.length; index++) {
+		const element = alarms[index];
+		element.playing = state === "on" ? true : false;
+		savePromises.push(alarmRepository.save(element));
+		triggerPromises.push(fetch(state === "on" ? `${element.ipAddress}/on` : `${element.ipAddress}/off`));
+	}
+	const saveRes = await Promise.all(savePromises);
+	const triggerRes = await Promise.all(triggerPromises);
+	const results = saveRes.concat(triggerRes);
+	return results;
 }
