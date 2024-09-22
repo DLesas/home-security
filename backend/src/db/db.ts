ddc3e161d69c879ad4b12550c2983d7775e56b35
DLesas <dlesas@hotmail.com>
2024-09-22 12:06:41 +0100

    feat: corrected indexes and added a new hypertable on sensorLogs table. Also added code to run creation of hypertables and indexes on startup.

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,7 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from "drizzle-orm";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import { db as schema } from "./schema/index";
+import fs from 'fs';
+import path from 'path';
+
 // for migrations
 // for query purposes
 const queryClient = postgres(
@@ -13,4 +17,12 @@ export async function runMigrations() {
 	console.log("Running migrations...");
 	await migrate(db, { migrationsFolder: "./drizzle" });
 	console.log("Migrations complete.");
+}
+
+export async function runCustomSQL() {
+	console.log("Running custom Startup SQL...");
+	const seedFilePath = path.join(__dirname, 'seed.sql');
+	const seedSQL = fs.readFileSync(seedFilePath, 'utf8');
+	await db.execute(sql.raw(seedSQL));
+	console.log("Custom SQL execution complete.");
 }
