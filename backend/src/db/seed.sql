b147de6696e7fd71631153ab9068bdd511b5594d
DLesas <dlesas@hotmail.com>
2024-09-22 12:06:41 +0100

    feat: corrected indexes and added a new hypertable on sensorLogs table. Also added code to run creation of hypertables and indexes on startup.

--- backend/src/db/seed.sql
+++ backend/src/db/seed.sql
@@ -14,28 +14,32 @@ BEGIN
   END IF;
 END $$;
 
--- Convert sensorlogs to hypertable
+-- Convert sensorUpdates to hypertable
 DO $$
 BEGIN
-  IF NOT _timescaledb_internal.is_hypertable('sensorLogs') THEN
-    PERFORM create_hypertable('sensorLogs', by_range('dateTime', INTERVAL '1 day'));
+  IF NOT _timescaledb_internal.is_hypertable('sensorUpdates') THEN
+    PERFORM create_hypertable('sensorUpdates', by_range('dateTime', INTERVAL '1 day'));
   END IF;
 END $$;
 
--- Create index on sensorLogs for building name and time
+-- Create index on sensorUpdates for sensor name, building name, and time
 DO $$
 BEGIN
   IF NOT EXISTS (
     SELECT 1
     FROM pg_indexes
-    WHERE indexname = 'sensor_logs_building_name_time_idx'
+    WHERE indexname = 'sensor_updates_sensor_name_building_name_time_idx'
   ) THEN
-    CREATE INDEX sensor_logs_building_name_time_idx
-    ON sensorLogs (dateTime, (
-      SELECT buildings.name
-      FROM doorSensors
-      JOIN buildings ON doorSensors.buildingId = buildings.id
-      WHERE doorSensors.id = sensorLogs.sensorId
+    CREATE INDEX sensor_updates_sensor_name_building_name_time_idx
+    ON sensorUpdates (dateTime, (
+      SELECT ds.name
+      FROM doorSensors ds
+      WHERE ds.id = sensorUpdates.sensorId
+    ), (
+      SELECT b.name
+      FROM doorSensors ds
+      JOIN buildings b ON ds.buildingId = b.id
+      WHERE ds.id = sensorUpdates.sensorId
     ));
   END IF;
 END $$;
@@ -45,5 +49,37 @@ DO $$
 BEGIN
   IF NOT _timescaledb_internal.is_hypertable('accessLogs') THEN
     PERFORM create_hypertable('accessLogs', by_range('dateTime', INTERVAL '3 days'));
+  END IF;
+END $$;
+
+
+-- Convert sensorLogs to hypertable
+DO $$
+BEGIN
+  IF NOT _timescaledb_internal.is_hypertable('sensorLogs') THEN
+    PERFORM create_hypertable('sensorLogs', by_range('dateTime', INTERVAL '3 days'));
+  END IF;
+END $$;
+
+
+-- Create index on sensorLogs for sensor name and building name
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_indexes
+    WHERE indexname = 'sensor_logs_sensor_name_building_name_idx'
+  ) THEN
+    CREATE INDEX sensor_logs_sensor_name_building_name_idx
+    ON sensorLogs (dateTime, (
+      SELECT ds.name
+      FROM doorSensors ds
+      WHERE ds.id = sensorLogs.sensorId
+    ), (
+      SELECT b.name
+      FROM doorSensors ds
+      JOIN buildings b ON ds.buildingId = b.id
+      WHERE ds.id = sensorLogs.sensorId
+    ));
   END IF;
 END $$;
