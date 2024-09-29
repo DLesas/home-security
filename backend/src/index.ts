55c947da2e76502f12351609b07fcea992311791
DLesas <dlesas@hotmail.com>
2024-09-29 14:16:06 +0100

    fix: fixed redis not properly initialising

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,30 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
-import { errorHandler, loggingMiddleware } from "./express/middleware";
-import sensorRoutes from "./express/routes/Sensors";
-import buildingRoutes from "./express/routes/Buildings";
-import alarmRoutes from "./express/routes/Alarms";
-import logRoutes from "./express/routes/Logs";
-import setupSocketHandlers from "./express/socketHandler";
-import { runMigrations, runCustomSQL } from "./db/db";
+import { errorHandler, loggingMiddleware } from "./express/middleware.js";
+import sensorRoutes from "./express/routes/Sensors.js";
+import buildingRoutes from "./express/routes/Buildings.js";
+import alarmRoutes from "./express/routes/Alarms.js";
+import logRoutes from "./express/routes/Logs.js";
+import setupSocketHandlers from "./express/socketHandler.js";
+import { runMigrations, runCustomSQL } from "./db/db.js";
+import { connectRedis } from "./redis/index.js";
+import { createDoorSensorIndex } from "./redis/doorSensors.js";
+import { createConfigIndex, setDefaultConfig } from "./redis/config.js";
+import { createAlarmIndex } from "./redis/alarms.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 const port = process.env.PORT || 8080;
 
-await runMigrations(); // run migrations to make tables
-await runCustomSQL(); // run custom sql to make hypertables and indexes
-
+await connectRedis();
+await createDoorSensorIndex();
+await createConfigIndex();
+await createAlarmIndex();
+await setDefaultConfig();
+await runMigrations();
+// await runCustomSQL();
 
 app.use(express.json());
 app.use(loggingMiddleware);
--- backend/src/redis/index.ts
+++ backend/src/redis/index.ts
@@ -2,7 +2,15 @@ import { createClient } from "redis";
 import { Repository, Schema } from "redis-om";
 
 export const redis = createClient({
-  url: `redis://:${process.env.REDIS_PASSWORD}@redis:6379`
+  url: `redis://redis:6379`
 });
-redis.on("error", (error: string) => console.error(error));
-await redis.connect();
+redis.on("error", (error: string) => console.error('Redis Client Error', error));
+export const connectRedis = async () => {
+  try {
+    await redis.connect();
+    console.log("Connected to Redis successfully");
+  } catch (error) {
+    console.error("Failed to connect to Redis:", error);
+    throw error;
+  }
+};
