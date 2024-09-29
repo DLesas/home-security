a5afd5ccbe827d8e754e0bc487871fa5d3b132a0
DLesas <dlesas@hotmail.com>
2024-09-29 14:16:06 +0100

    fix: fixed redis not properly initialising

--- backend/src/redis/config.ts
+++ backend/src/redis/config.ts
@@ -1,12 +1,12 @@
 import { Repository, Schema } from "redis-om";
-import { redis } from ".";
+import { redis } from "./index.js";
+import { raiseError } from "../errorHandling.js";
 
 const configSchema = new Schema("config", {
   sensorWarningTemparature: { type: "number" },
   sensorCriticalTemparature: { type: "number" },
   pushTokens: { type: "string[]" },
 });
-
 export interface Config {
   sensorWarningTemparature: number;
   sensorCriticalTemparature: number;
@@ -15,4 +15,27 @@ export interface Config {
 
 export const configRepository = new Repository(configSchema, redis);
 
-await configRepository.createIndex();
+export const createConfigIndex = async () => {
+  try {
+    await configRepository.createIndex();
+  } catch (error) {
+    console.error('Error creating config index:', error);
+  }
+};
+
+
+export const setDefaultConfig = async () => {
+  const config = (await configRepository.search().returnFirst()) as Config | null;
+  if (config) {
+    return;
+  }
+  if (!process.env.SENSOR_WARNING_TEMPERATURE || !process.env.SENSOR_CRITICAL_TEMPERATURE) {
+    raiseError(500, "SENSOR_WARNING_TEMPERATURE or SENSOR_CRITICAL_TEMPERATURE is not set");
+    return;
+  }
+  await configRepository.save({
+    sensorWarningTemparature: parseInt(process.env.SENSOR_WARNING_TEMPERATURE!),
+    sensorCriticalTemparature: parseInt(process.env.SENSOR_CRITICAL_TEMPERATURE!),
+    pushTokens: [],
+  } as Config);
+};
