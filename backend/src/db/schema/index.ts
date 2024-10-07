35732706fb0c49ae6677106c6db79ed26dec4c5d
DLesas <dlesas@hotmail.com>
2024-10-07 16:06:13 +0100

    see previous

--- backend/src/db/schema/index.ts
+++ backend/src/db/schema/index.ts
@@ -1,16 +1,18 @@
 import { doorSensorStateEnum, sensorUpdatesTable } from "./sensorUpdates.js";
-import { doorSensorsTable } from "./doorSensors.js";
+import { sensorsTable } from "./sensors.js";
 import { buildingTable as buildingsTable } from "./buildings.js";
 import { eventLogsTable, eventTypeEnum } from "./eventLogs.js";
 import { accessLogsTable, actionEnum, connectionEnum } from "./accessLogs.js";
 import { errorLogsTable } from "./errorLogs.js";
 import { sensorLogsTable } from "./sensorLogs.js";
 import { alarmLogsTable } from "./alarmLogs.js";
+import { alarmUpdatesTable, alarmStateEnum } from "./alarmUpdates.js";
+import { alarmsTable } from "./alarms.js";
 
 export const actionEnums = actionEnum;
 export const eventTypeEnums = eventTypeEnum;
 export const connectionEnums = connectionEnum;
-
+export const alarmStateEnums = alarmStateEnum;
 
 export const generalLogTable = accessLogsTable;
 export const errorLogTable = errorLogsTable;
@@ -18,13 +20,17 @@ export const eventLogTable = eventLogsTable;
 export const doorSensorState = doorSensorStateEnum;
 export const sensorUpdateTable = sensorUpdatesTable;
 export const sensorLogTable = sensorLogsTable;
-export const doorSensorTable = doorSensorsTable;
+export const doorSensorTable = sensorsTable;
 export const buildingTable = buildingsTable;
 export const alarmLogTable = alarmLogsTable;
+export const alarmUpdateTable = alarmUpdatesTable;
+export const alarmTable = alarmsTable;
+
 export const db = {
   actionEnums,
   eventTypeEnums,
   connectionEnums,
+  alarmStateEnums,
   errorLogTable,
   generalLogTable,
   eventLogTable,
@@ -34,4 +40,5 @@ export const db = {
   sensorLogTable,
   doorSensorTable,
   buildingTable,
+  alarmTable,
 };
