2c60bbc9948a622878d2d5479b7ce1db74486efc
DLesas <dlesas@hotmail.com>
2024-10-07 16:05:11 +0100

    [backend] feat: added akarm update table to be in line with door sensor table

---
+++ backend/src/db/schema/alarmUpdates.ts
@@ -0,0 +1,20 @@
+import { bigserial, integer, numeric, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { alarmsTable } from "./alarms.js";
+import { InferInsertModel, InferSelectModel } from "drizzle-orm";
+
+export const alarmStateEnum = pgEnum("alarmState", ["on", "off"]);
+
+export const alarmUpdatesTable = pgTable("alarmUpdates", {
+	id: bigserial("id", { mode: "number" }).primaryKey(),
+	alarmId: text("alarmId")
+		.notNull()
+		.references(() => alarmsTable.id, { onDelete: "cascade" }),
+	state: alarmStateEnum("state").notNull(),
+	temperature: numeric("temperature", { precision: 5, scale: 2 }).notNull(),
+	voltage: numeric("voltage", { precision: 5, scale: 2 }),
+	frequency: integer("frequency"),
+	dateTime: timestamp("dateTime", { withTimezone: true }).defaultNow(),
+});
+
+export type selectAlarmUpdate = InferSelectModel<typeof alarmUpdatesTable>;
+export type insertAlarmUpdate = InferInsertModel<typeof alarmUpdatesTable>;
