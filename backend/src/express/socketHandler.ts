51105867050df79e205e148dfc258d38f55a5d51
DLesas <dlesas@hotmail.com>
2024-10-07 16:11:11 +0100

    [backend] feat: added correct emition of changes to clients via sockets

--- backend/src/express/socketHandler.ts
+++ backend/src/express/socketHandler.ts
@@ -1,16 +1,20 @@
 import { io } from "../index.js";
 import { Server } from "socket.io";
 import { type doorSensor, doorSensorRepository } from "../redis/doorSensors.js";
+import { type Alarm, alarmRepository } from "../redis/alarms.js";
+
 
 export async function emitNewData() {
 	const sensors = (await doorSensorRepository.search().returnAll()) as doorSensor[];
-	io.emit("data", sensors);
+	const alarms = (await alarmRepository.search().returnAll()) as Alarm[];
+	io.emit("data", {sensors, alarms});
 }
 
 const setupSocketHandlers = (io: Server) => {
 	io.on("connection", async (socket) => {
 		const sensors = (await doorSensorRepository.search().returnAll()) as doorSensor[];
-		socket.emit("data", sensors);
+		const alarms = (await alarmRepository.search().returnAll()) as Alarm[];
+		socket.emit("data", {sensors, alarms});
 
 		// Add more socket event handlers here
 		// For example:
