d2e617419e38e40bced98c3363f11f2cc8a6988c
DLesas <dlesas@hotmail.com>
2024-10-25 18:28:20 +0100

    [microdevices] refactor: updated parent classes and main functions of both alarm relays and door sensors due to previous changes

--- device_code/circuit_python/doorSensor/main.py
+++ device_code/circuit_python/doorSensor/main.py
@@ -1,11 +1,11 @@
 from doorSensor import DoorSensor
-from device import Device
+from microDevice import microDevice
 from led import Led
-from logger import Logger
+from logging import Logger
 from timeClock import TimeClock
 from udp import Udp
 from networking import Networking
-from wifi import Wifi
+from deviceWifi import deviceWifi
 from utils import read_env_file
 import time
 
@@ -15,50 +15,50 @@ import time
 
 def main_light_sleep():
     env = read_env_file()
-    Device = Device()
-    Led = Led()
-    Logger = Logger(Device)
-    Wifi = Wifi(Logger, Led, env["ssid"], env["password"], env["server_ip"], env["server_port"], env["max_wifi_attempts"], env["wifi_blinks"], env["id"])
-    Wifi.connect()
-    RTC = TimeClock(Wifi, Logger, Led)
-    RTC.set_time_ntp()
-    Udp = Udp(Wifi, Logger, Led, env["server_udp_port"], env["server_service_name"], env["server_password"], env["tcp_timeout"], env["tcp_port"])
-    Networking = Networking(Wifi, Logger, Led, Udp, env["max_networking_attempts"], env["networking_blinks"], env["server_ip"], env["server_port"], env["server_ssl"], env["api_version"], env["deviceType"], env["user_agent"])
-    Networking.find_server()
-    Networking.handshake_with_server()
-    DoorSensor = DoorSensor(Logger, Led, Device, Wifi, Networking, env["door_switch_pin"], env["time_to_sleep_s"])
+    Device = microDevice()
+    led = Led()
+    logger = Logger(Device)
+    DeviceWifi = deviceWifi(logger, led, env["ssid"], env["password"], env["server_ip"], env["server_port"], env["max_wifi_attempts"], env["wifi_blinks"], env["id"])
+    DeviceWifi.connect()
+    timeclock = TimeClock(logger, led, DeviceWifi)
+    timeclock.set_time_ntp()
+    udp = Udp(DeviceWifi, logger, led, env["server_udp_port"], env["server_service_name"], env["server_password"], env["tcp_timeout"], env["tcp_port"])
+    networking = Networking(DeviceWifi, Device, logger, led, udp, env["max_networking_attempts"], env["networking_blinks"], env["server_ip"], env["server_port"], env["server_ssl"], env["api_version"], env["deviceType"], env["user_agent"])
+    networking.find_server()
+    networking.handshake_with_server()
+    doorSensor = DoorSensor(logger, led, Device, DeviceWifi, networking, env["door_switch_pin"], env["time_to_sleep_s"])
     while True:
-        Wifi.check_connection()
-        DoorSensor.read_all_stats()
-        DoorSensor.send_data()
-        needsClearing = Logger.check_log_files()
+        DeviceWifi.check_connection()
+        doorSensor.read_all_stats()
+        doorSensor.send_data()
+        needsClearing = logger.check_log_files()
         if needsClearing:
-            Networking.send_logs()
-        DoorSensor.light_sleep()
+            networking.send_logs()
+        doorSensor.light_sleep()
     
     
 
 def main_deep_sleep():
     env = read_env_file()
-    Device = Device()
-    Led = Led()
-    Logger = Logger(Device)
-    Wifi = Wifi(Logger, Led, env["ssid"], env["password"], env["server_ip"], env["server_port"], env["max_wifi_attempts"], env["wifi_blinks"], env["id"])
-    Wifi.connect()
-    RTC = TimeClock(Wifi, Logger, Led)
-    RTC.set_time_ntp()
-    Udp = Udp(Wifi, Logger, Led, env["server_udp_port"], env["server_service_name"], env["server_password"], env["tcp_timeout"], env["tcp_port"])
-    Networking = Networking(Wifi, Logger, Led, Udp, env["max_networking_attempts"], env["networking_blinks"], env["server_ip"], env["server_port"], env["server_ssl"], env["api_version"], env["deviceType"], env["user_agent"])
-    Networking.find_server()
-    Networking.handshake_with_server()
-    DoorSensor = DoorSensor(Logger, Led, Device, Wifi, Networking, env["door_switch_pin"], env["time_to_sleep_s"])
-    Wifi.check_connection()
-    DoorSensor.read_all_stats()
-    DoorSensor.send_data()
-    needsClearing = Logger.check_log_files()
+    Device = microDevice()
+    led = Led()
+    logger = Logger(Device)
+    DeviceWifi = deviceWifi(logger, led, env["ssid"], env["password"], env["server_ip"], env["server_port"], env["max_wifi_attempts"], env["wifi_blinks"], env["id"])
+    DeviceWifi.connect()
+    timeclock = TimeClock(logger, led, DeviceWifi)
+    timeclock.set_time_ntp()
+    udp = Udp(DeviceWifi, logger, led, env["server_udp_port"], env["server_service_name"], env["server_password"], env["tcp_timeout"], env["tcp_port"])
+    networking = Networking(DeviceWifi, Device, logger, led, udp, env["max_networking_attempts"], env["networking_blinks"], env["server_ip"], env["server_port"], env["server_ssl"], env["api_version"], env["deviceType"], env["user_agent"])
+    networking.find_server()
+    networking.handshake_with_server()
+    doorSensor = DoorSensor(logger, led, Device, DeviceWifi, networking, env["door_switch_pin"], env["time_to_sleep_s"])
+    DeviceWifi.check_connection()
+    doorSensor.read_all_stats()
+    doorSensor.send_data()
+    needsClearing = logger.check_log_files()
     if needsClearing:
-        Networking.send_logs()
-    DoorSensor.deep_sleep()
+        networking.send_logs()
+    doorSensor.deep_sleep()
 
 
 if __name__ == "__main__":
