d2954c75f28a87a695be7414a644733ec72b2eed
DLesas <dlesas@hotmail.com>
2024-10-25 17:52:12 +0100

    [admin app] feat: added next ui, some shadcn components

--- adminApp/tailwind.config.js
+++ adminApp/tailwind.config.js
@@ -1,11 +1,14 @@
+const { nextui } = require('@nextui-org/theme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,jsx,ts,tsx,html}',
+    './node_modules/@nextui-org/theme/dist/components/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [nextui()]
 }
-
