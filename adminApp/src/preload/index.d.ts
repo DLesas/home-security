e7f732e6d69f82a2a64341c8e79d8f76d8c480cf
DLesas <dlesas@hotmail.com>
2024-12-04 19:09:05 +0000

    see previous commit

--- adminApp/src/preload/index.d.ts
+++ adminApp/src/preload/index.d.ts
@@ -1,8 +1,76 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+type API = {
+  saveEnv: (data: any, devicePath: any) => void
+  onSaveEnvResult: (callback: any) => Electron.IpcRenderer
+  getUSBDevices: () => Promise<{
+    vendorId: number
+    productId: number
+    manufacturer: string
+    product: string
+  }[]>
+  onUsbDevicesUpdated: (
+    callback: (
+      event: Electron.IpcRendererEvent,
+      devices: {
+        vendorId: number
+        productId: number
+        manufacturer: string
+        product: string
+      }[]
+    ) => void
+  ) => void
+  removeUsbDevicesUpdatedListener: (
+    callback: (
+      event: Electron.IpcRendererEvent,
+      devices: {
+        vendorId: number
+        productId: number
+        manufacturer: string
+        product: string
+      }[]
+    ) => void
+  ) => void
+  onWifiNetworksChanged: (
+    callback: (
+      event: Electron.IpcRendererEvent,
+      networks: {
+        isConnected: boolean
+        ssid: string
+        quality: number
+        security: string
+        frequency: number
+        signal_level: number
+      }[]
+    ) => void
+  ) => void
+  removeWifiNetworksChangedListener: (
+    callback: (
+      event: Electron.IpcRendererEvent,
+      networks: {
+        isConnected: boolean
+        ssid: string
+        quality: number
+        security: string
+        frequency: number
+        signal_level: number
+      }[]
+    ) => void
+  ) => void
+  getWifiNetworks: () => Promise<{
+    isConnected: boolean
+    ssid: string
+    quality: number
+    security: string
+    frequency: number
+    signal_level: number
+  }[]>
+  getLocalIpAddress: () => Promise<string>
+}
+
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    api: API
   }
 }
--- adminApp/src/preload/index.ts
+++ adminApp/src/preload/index.ts
@@ -1,8 +1,20 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  saveEnv: (data, devicePath) => ipcRenderer.send('save-env', data, devicePath),
+  onSaveEnvResult: (callback) => ipcRenderer.on('save-env-result', (_, result) => callback(result)),
+  getUSBDevices: () => ipcRenderer.invoke('get-usb-devices'),
+  onUsbDevicesUpdated: (callback) => ipcRenderer.on('usb-devices-updated', callback),
+  removeUsbDevicesUpdatedListener: (callback) => ipcRenderer.removeListener('usb-devices-updated', callback),
+  onWifiNetworksChanged: (callback) => ipcRenderer.on('wifi-networks-changed', callback),
+  removeWifiNetworksChangedListener: (callback) => ipcRenderer.removeListener('wifi-networks-changed', callback),
+  getWifiNetworks: () => ipcRenderer.invoke('get-wifi-networks'),
+  getLocalIpAddress: () => ipcRenderer.invoke('get-local-ip-address'),
+}
+
+
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
