fdecb0f5e373a1ebb7fa62ec12365f89437e6d1a
DLesas <dlesas@hotmail.com>
2025-08-05 17:14:14 +0100

    [backend]: feat: add 'unknown' value to alarmState enum and update related schema

---
+++ localServices/backend/db/drizzle/0007_bitter_ben_grimm.sql
@@ -0,0 +1,1 @@
+ALTER TYPE "alarmState" ADD VALUE 'unknown';
--- localServices/backend/src/db/schema/alarmUpdates.ts
+++ localServices/backend/src/db/schema/alarmUpdates.ts
@@ -1,19 +1,27 @@
-import { bigserial, integer, numeric, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+  bigserial,
+  integer,
+  numeric,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { alarmsTable } from "./alarms";
 import { InferInsertModel, InferSelectModel } from "drizzle-orm";
 
-export const alarmStateEnum = pgEnum("alarmState", ["on", "off"]);
+export const alarmStateEnum = pgEnum("alarmState", ["on", "off", "unknown"]);
 
 export const alarmUpdatesTable = pgTable("alarmUpdates", {
-	id: bigserial("id", { mode: "number" }).primaryKey(),
-	alarmId: text("alarmId")
-		.notNull()
-		.references(() => alarmsTable.id, { onDelete: "cascade" }),
-	state: alarmStateEnum("state").notNull(),
-	temperature: numeric("temperature", { precision: 5, scale: 2 }),
-	voltage: numeric("voltage", { precision: 5, scale: 2 }),
-	frequency: integer("frequency"),
-	dateTime: timestamp("dateTime", { withTimezone: true }).defaultNow(),
+  id: bigserial("id", { mode: "number" }).primaryKey(),
+  alarmId: text("alarmId")
+    .notNull()
+    .references(() => alarmsTable.id, { onDelete: "cascade" }),
+  state: alarmStateEnum("state").notNull(),
+  temperature: numeric("temperature", { precision: 5, scale: 2 }),
+  voltage: numeric("voltage", { precision: 5, scale: 2 }),
+  frequency: integer("frequency"),
+  dateTime: timestamp("dateTime", { withTimezone: true }).defaultNow(),
 });
 
 export type selectAlarmUpdate = InferSelectModel<typeof alarmUpdatesTable>;
