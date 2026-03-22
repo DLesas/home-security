9e0b4640d4a8e99d6e8dcf995926d829c749332e
Jonjo <jonjo22@hotmail.com>
2026-03-22 19:27:13 +0000

    [webapp]: feat: add confirmation modal before testing alarm
    Show a warning modal when the test alarm button is pressed to prevent
    accidental triggers. Reuses the existing ArmWarningModal component.

--- localServices/webapp/app/alarms/components/AlarmFloatingControls.tsx
+++ localServices/webapp/app/alarms/components/AlarmFloatingControls.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { Button } from '@nextui-org/button'
+import { useDisclosure } from '@nextui-org/modal'
 import { useTestAlarmMutation } from '../../../hooks/mutations/useAlarmMutations'
+import { ArmWarningModal } from '../../home/components/ArmWarningModal'
 
 interface AlarmFloatingControlsProps {
   alarmId: string
@@ -10,20 +12,33 @@ interface AlarmFloatingControlsProps {
 
 export function AlarmFloatingControls({ alarmId, alarmName }: AlarmFloatingControlsProps) {
   const testMutation = useTestAlarmMutation()
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
   return (
-    <div className="fixed bottom-6 left-0 right-0 px-4 z-10">
-      <div className="container mx-auto max-w-4xl">
-        <Button
-          color="danger"
-          size="lg"
-          className="w-full shadow-lg"
-          isLoading={testMutation.isPending}
-          onPress={() => testMutation.mutate(alarmId)}
-        >
-          Test Alarm
-        </Button>
+    <>
+      <div className="fixed bottom-6 left-0 right-0 px-4 z-10">
+        <div className="container mx-auto max-w-4xl">
+          <Button
+            color="danger"
+            size="lg"
+            className="w-full shadow-lg"
+            isLoading={testMutation.isPending}
+            onPress={onOpen}
+          >
+            Test Alarm
+          </Button>
+        </div>
       </div>
-    </div>
+
+      <ArmWarningModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        title="Test alarm?"
+        message={`This will briefly sound ${alarmName}. Are you sure you want to continue?`}
+        onConfirm={() => testMutation.mutate(alarmId)}
+        onCancel={() => {}}
+        confirmText="Test alarm"
+      />
+    </>
   )
 }
